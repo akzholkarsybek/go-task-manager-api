8d6f1d39efb931e7cc03cece170b79620aac47e3
Akzhol Karsybek <aka@MacBook-Air-Akzhol.local>
2026-07-06 11:57:39 +0500

    feat(task): add task model

--- internal/model/task.go
+++ internal/model/task.go
@@ -7,7 +7,7 @@ type Task struct{
 	Title string `json:"title"`
 	Description string `json:"description"`
 	Completed bool `json:"completed"`
-	DueDate time.Time `json:"due_date"`
+	DueDate *time.Time `json:"due_date"`
 	UserID int64 `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 }
