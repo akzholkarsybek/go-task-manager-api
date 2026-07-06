c320f5327be3b826fe037f2931f2497d3a91c5a2
Akzhol Karsybek <aka@MacBook-Air-Akzhol.local>
2026-07-06 12:14:43 +0500

    feat(auth): add JWT helper functions

---
+++ internal/auth/jwt.go
@@ -0,0 +1,19 @@
+package auth
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("CHANGE_ME_SECRET")
+
+func generateJWT(userID int64, role string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
