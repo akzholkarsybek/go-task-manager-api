3065589c8b3447b8954fb0e25abcf9e5648396c8
Akzhol Karsybek <aka@MacBook-Air-Akzhol.local>
2026-07-06 11:56:15 +0500

    feat(task): implement task repository CRUD operations

--- internal/repository/postgres_task_repository.go
+++ internal/repository/postgres_task_repository.go
@@ -2,20 +2,22 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
+
 	"github.com/Akakazkz/go-task-manager-api/internal/model"
 )
 
-type postgresTaskRepository struct{
+type postgresTaskRepository struct {
 	db *sql.DB
 }
 
-func NewPostgresTaskRepository(db *sql.DB) TaskRepository{
+func NewPostgresTaskRepository(db *sql.DB) TaskRepository {
 	return &postgresTaskRepository{
-		db: db, 
+		db: db,
 	}
 }
 
-func (r *postgresTaskRepository) Create(task *model.Task) error{
+func (r *postgresTaskRepository) Create(task *model.Task) error {
 	query := `
 		INSERT INTO tasks ( title, description, completed, due_date, user_id, created_at) 
 		VALUES ($1, $2, $3, $4, $5, $6) 
@@ -31,27 +33,47 @@ func (r *postgresTaskRepository) Create(task *model.Task) error{
 		task.CreatedAt,
 	).Scan(&task.ID)
 }
-func (r *postgresTaskRepository) GetByID(id, userID int64) (*model.Task, error){
-	panic("not implemented")
+func (r *postgresTaskRepository) GetByID(id, userID int64) (*model.Task, error) {
+	var task model.Task
+	query := `
+		SELECT id, title, description, completed, due_date, user_id, created_at 
+		FROM tasks
+		WHERE id = $1
+		AND user_id = $2
+	`
+	err := r.db.QueryRow(query, id, userID).Scan(
+		&task.ID,
+		&task.Title,
+		&task.Description,
+		&task.Completed,
+		&task.DueDate,
+		&task.UserID,
+		&task.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &task, nil
 }
 
-func (r *postgresTaskRepository) ListByUserID(userID int64) ([]*model.Task, error){
+func (r *postgresTaskRepository) ListByUserID(userID int64) ([]*model.Task, error) {
 	rows, err := r.db.Query(`
 		SELECT id, title, description, completed, due_date, user_id, created_at
 		FROM tasks
 		WHERE user_id = $1
 		ORDER BY created_at DESC
 	`,
-	userID,
-)
-	if err != nil{
+		userID,
+	)
+	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	var tasks []*model.Task
 
-	for rows.Next(){
+	for rows.Next() {
 		var t model.Task
 		if err := rows.Scan(
 			&t.ID,
@@ -61,22 +83,68 @@ func (r *postgresTaskRepository) ListByUserID(userID int64) ([]*model.Task, erro
 			&t.DueDate,
 			&t.UserID,
 			&t.CreatedAt,
-		); err != nil{
+		); err != nil {
 			return nil, err
 		}
 		tasks = append(tasks, &t)
 	}
 	if err := rows.Err(); err != nil {
-    	return nil, err
+		return nil, err
 	}
 	return tasks, nil
 
 }
-func (r *postgresTaskRepository) Update(task *model.Task) error{
-	panic("not implemented")
-}
-func (r *postgresTaskRepository) Delete(id, userID int64) error{
-	panic("not implemented")
-}
+func (r *postgresTaskRepository) Update(task *model.Task) error {
+	result, err := r.db.Exec(`
+	UPDATE tasks
+	SET
+		title = $1,
+		description = $2,
+		completed = $3,
+		due_date = $4
+	WHERE id = $5
+	AND user_id = $6
+	`,
+		task.Title,
+		task.Description,
+		task.Completed,
+		task.DueDate,
+		task.ID,
+		task.UserID,
+	)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
 
+	if err != nil {
+		return err
+	}
 
+	if rowsAffected == 0 {
+		return errors.New("task not found")
+	}
+	return nil
+
+}
+func (r *postgresTaskRepository) Delete(id, userID int64) error {
+	results, err := r.db.Exec(`
+	DELETE FROM tasks
+	WHERE id = $1
+	AND user_id = $2
+	`,
+	id,
+	userID,
+	)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := results.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0{
+		return errors.New("task not found")
+	}
+	return nil
+}
